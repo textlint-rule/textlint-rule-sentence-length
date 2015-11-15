baf8446685b5eb568ec6515c1a18fca864da1fac
azu <azuciao@gmail.com>
2015-11-15 23:33:38 +0900

    initial commit

---
+++ src/sentence-length.js
@@ -0,0 +1,12 @@
+// LICENSE : MIT
+"use strict";
+import sentenceSplitter from "sentence-splitter";
+const defaultOptions = {
+    max: 100
+};
+export default function (context, options = {}) {
+    const maxLength = otpions.max || defaultOptions.max;
+    // toPlainText
+    return {
+    }
+}

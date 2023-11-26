5b0a27ca757e86ad7ee8882e4e0d0e65a21dc5d3
azu <azuciao@gmail.com>
2023-11-26 10:14:59 +0900

    refactor(deps): update to setence-splitter@5
    **BREAKING CHANGES**
    
    > [!WARNING]
    > Require Node.js 18+
    
    - Update to sentence-splitter@5
      - https://github.com/textlint-rule/sentence-splitter/releases/tag/v5.0.0

--- test/sentence-length-test.ts
+++ test/sentence-length-test.ts
@@ -1,7 +1,5 @@
 import TextLintTester from "textlint-tester";
 import rule from "../src/sentence-length";
-// @ts-expect-error: no types
-import htmlPlugin from "textlint-plugin-html";
 
 const tester = new TextLintTester();
 
@@ -254,50 +252,3 @@ Over 18 characters.`
         }
     ]
 });
-
-tester.run(
-    "textlint-rule-sentence-length:plugin",
-    {
-        plugins: [
-            {
-                pluginId: "html",
-                plugin: htmlPlugin
-            }
-        ],
-        rules: [
-            {
-                ruleId: "textlint-rule-sentence-length",
-                rule: rule,
-                options: {
-                    max: 15
-                }
-            }
-        ]
-    },
-    {
-        valid: [
-            {
-                text: "<p>this is a test.</p>",
-                ext: ".html"
-            },
-            {
-                text: "<p>TEST is <a href='https://example.com'>https://example.com</a></p>",
-                ext: ".html"
-            }
-        ],
-        invalid: [
-            {
-                text: "<p>this is a test for textlint-rule-sentence-length with plugin</p>",
-                ext: ".html",
-                errors: [
-                    {
-                        message: `Line 1 sentence length(60) exceeds the maximum sentence length of 15.
-Over 45 characters.`,
-                        line: 1,
-                        column: 4
-                    }
-                ]
-            }
-        ]
-    }
-);
